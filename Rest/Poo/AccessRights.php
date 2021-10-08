000b9cfbd1be51cb8394e3b6865a3ea41a89234e
Esteban Baichoo <esteban.baichoo@gmail.com>
2021-10-08 17:06:24 +0200

    Cahngement d'ordre, parce que.

--- Rest/Poo/AccessRights.php
+++ Rest/Poo/AccessRights.php
@@ -2,11 +2,11 @@
 
 abstract class AccessRights
 {
-    const SAME_USER = 'SAME_USER';
     const PUBLIC_ACCESS = 'ALL_ACCESS';
+    const SAME_USER = 'SAME_USER';
     const GROUP_MEMBERS = 'GROUP_MEMBERS';
-    const ADMIN = 'ADMIN';
     const GAME_MASTER = 'GAME_MASTER';
+    const ADMIN = 'ADMIN';
     // etc.
 }
 
